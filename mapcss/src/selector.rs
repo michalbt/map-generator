98a06af32af4b6ed02a613d03d7032050d9b9ad7
Michal Bernat <michal.bernat.2005@gmail.com>
2026-08-31 00:35:13 +0200

    Start implementing selector parsing

--- mapcss/src/selector.rs
+++ mapcss/src/selector.rs
@@ -1,3 +1,4 @@
+use cssparser::Parser;
 use map_data::{AreaSource, ObjectKey, Storage};
 
 pub(crate) trait Selector {
@@ -100,6 +101,34 @@ enum ObjectType {
     Any,
 }
 
+impl ObjectType {
+    pub fn parse(input: &mut Parser) -> Result<Self, ()> {
+        fn parse_identifier_object_type(input: &mut Parser) -> Result<ObjectType, ()> {
+            if let Ok(identifier) = input.expect_ident() {
+                match &**identifier {
+                    "node" => Ok(ObjectType::Node),
+                    "way" => Ok(ObjectType::Way),
+                    "relation" => Ok(ObjectType::Relation),
+                    "area" => Ok(ObjectType::Area),
+                    "line" => Ok(ObjectType::Line),
+                    "canvas" => Ok(ObjectType::Canvas),
+                    _ => Err(()),
+                }
+            } else {
+                Err(())
+            }
+        }
+
+        if let Ok(object_type) = input.try_parse(parse_identifier_object_type) {
+            Ok(object_type)
+        } else if let Ok(()) = input.expect_delim('*') {
+            Ok(ObjectType::Any)
+        } else {
+            Err(())
+        }
+    }
+}
+
 impl Selector for ObjectType {
     fn matches(&self, key: ObjectKey, storage: &Storage) -> bool {
         match (*self, key) {
@@ -129,11 +158,17 @@ enum Test {
     NotSet(TagKey),
     Equal(TagKey, TagValue),
     NotEqual(TagKey, TagValue),
-    LessThan(TagKey, TagValue),
-    LessThanOrEqual(TagKey, TagValue),
-    GreaterThan(TagKey, TagValue),
-    GreaterThanOrEqual(TagKey, TagValue),
+    LessThan(TagKey, TagNumericValue),
+    LessThanOrEqual(TagKey, TagNumericValue),
+    GreaterThan(TagKey, TagNumericValue),
+    GreaterThanOrEqual(TagKey, TagNumericValue),
     Matches(TagKey, Regex),
+}
+
+impl Test {
+    pub fn parse(input: &mut Parser) -> Result<Self, ()> {
+        todo!()
+    }
 }
 
 impl Selector for Test {
@@ -145,8 +180,79 @@ impl Selector for Test {
 #[derive(Clone, Debug, PartialEq)]
 struct TagKey(pub String);
 
-#[derive(Clone, Debug, PartialEq)]
-struct TagValue(pub String);
+impl TagKey {
+    pub fn parse(input: &mut Parser) -> Result<Self, ()> {
+        input
+            .expect_ident_or_string()
+            .map(|s| TagKey(s.to_string()))
+            .map_err(|_| ())
+    }
+}
 
 #[derive(Clone, Debug, PartialEq)]
-struct Regex(pub String);
+enum TagValue {
+    Numeric(TagNumericValue),
+    String(TagStringValue),
+}
+
+impl TagValue {
+    pub fn parse(input: &mut Parser) -> Result<Self, ()> {
+        if let Ok(numeric) = input.try_parse(TagNumericValue::parse) {
+            Ok(Self::Numeric(numeric))
+        } else if let Ok(string) = TagStringValue::parse(input) {
+            Ok(Self::String(string))
+        } else {
+            Err(())
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+enum TagNumericValue {
+    Integer(i32),
+    Float(f32),
+}
+
+impl TagNumericValue {
+    pub fn parse(input: &mut Parser) -> Result<Self, ()> {
+        if let Ok(integer) = input.try_parse(|inp| inp.expect_integer()) {
+            Ok(Self::Integer(integer))
+        } else if let Ok(float) = input.expect_number() {
+            Ok(Self::Float(float))
+        } else {
+            Err(())
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct TagStringValue(pub String);
+
+impl TagStringValue {
+    pub fn parse(input: &mut Parser) -> Result<Self, ()> {
+        input
+            .expect_ident_or_string()
+            .map(|s| TagStringValue(s.to_string()))
+            .map_err(|_| ())
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Regex(pub regex::Regex);
+
+impl Regex {
+    /// Only quoted regexes are supported
+    pub fn parse(input: &mut Parser) -> Result<Self, ()> {
+        input
+            .expect_string()
+            .map_err(|_| ())
+            .and_then(|s| regex::Regex::new(s).map_err(|_| ()))
+            .map(Regex)
+    }
+}
+
+impl PartialEq for Regex {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.as_str() == other.0.as_str()
+    }
+}
